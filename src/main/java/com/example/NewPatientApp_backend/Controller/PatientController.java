55f37df6734e66ec1055b1af010b47cc3f01e01d
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-07 02:43:14 +0530

    model commit

--- src/main/java/com/example/NewPatientApp_backend/Controller/PatientController.java
+++ src/main/java/com/example/NewPatientApp_backend/Controller/PatientController.java
@@ -1,17 +1,33 @@
 package com.example.NewPatientApp_backend.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.NewPatientApp_backend.Model.Patient;
+import com.example.NewPatientApp_backend.dao.PatientDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PatientController {
 
-    @GetMapping("/add")
-    public String addPatient(){
+    @Autowired
+    private PatientDao dao;
+
+
+    @CrossOrigin (origins = "*")
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String addPatient(@RequestBody Patient p){
+        System.out.println(p.getName().toString());
+        dao.save(p);
         return "patient add page";
     }
+
+    @CrossOrigin(origins = "*")
     @GetMapping("/view")
-    public String viewPatient(){
-        return "patient view page";
+    public List<Patient> View()
+    {
+        return (List<Patient>)dao.findAll();
     }
-}
+    }
+
+
