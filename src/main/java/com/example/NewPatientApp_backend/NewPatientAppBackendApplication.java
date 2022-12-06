a3b93899382c5ffbb7302ceeea8cb26ca72a4bab
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-07 02:15:20 +0530

    initial commit

---
+++ src/main/java/com/example/NewPatientApp_backend/NewPatientAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewPatientApp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPatientAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewPatientAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NewPatientApp_backend/NewPatientAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewPatientApp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPatientAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
