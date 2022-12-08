aec953918613c8bc3f80e029ba298fb1c9910791
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-08 15:23:12 +0530

    delete commit

--- src/main/java/com/example/NewPatientApp_backend/dao/PatientDao.java
+++ src/main/java/com/example/NewPatientApp_backend/dao/PatientDao.java
@@ -1,6 +1,8 @@
 package com.example.NewPatientApp_backend.dao;
 
 import com.example.NewPatientApp_backend.Model.Patient;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +12,10 @@ import java.util.List;
 public interface PatientDao extends CrudRepository<Patient,Integer> {
 
     @Query(value = "SELECT `id`, `address`, `dateofapmt`, `dname`, `mobile`, `name` FROM `patient` WHERE `name`LIKE %:name%", nativeQuery = true)
-
     List<Patient>SearchPatient(@Param("name") String name);
+
+@Modifying
+@Transactional
+    @Query(value = "DELETE FROM `patient` WHERE `id`=:id",nativeQuery = true)
+    void DeletePatient(@Param("id") Integer id);
 }
