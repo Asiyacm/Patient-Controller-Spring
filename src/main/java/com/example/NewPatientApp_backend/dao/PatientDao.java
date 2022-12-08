9bd441221b322518e2a131068c84f57c333e026c
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-08 14:59:00 +0530

    searcha commit

--- src/main/java/com/example/NewPatientApp_backend/dao/PatientDao.java
+++ src/main/java/com/example/NewPatientApp_backend/dao/PatientDao.java
@@ -1,7 +1,15 @@
 package com.example.NewPatientApp_backend.dao;
 
 import com.example.NewPatientApp_backend.Model.Patient;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PatientDao extends CrudRepository<Patient,Integer> {
+
+    @Query(value = "SELECT `id`, `address`, `dateofapmt`, `dname`, `mobile`, `name` FROM `patient` WHERE `name`LIKE %:name%", nativeQuery = true)
+
+    List<Patient>SearchPatient(@Param("name") String name);
 }
